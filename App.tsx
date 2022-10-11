98d0dfc3d28eb4d7c9ef2eeb969212f04256cdd0
JasonKph <jason.jbell@yahoo.com>
2022-10-11 08:55:16 -0700

    playing around with react native views

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
+// import * as ReactNative from 'react-native'
 export default function App() {
-  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Hello TestyMcTest!!!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello TestyMcTest!!!</Text>
+      <Text style={styles.content}>Some text below the title that would serve to describe information about something relevant to said title and would contain the very information describing forementioned things.</Text>
+      <Text style={styles.title}>Another Title</Text>
+      <Text style={styles.content}>Some text below the title</Text>
+      <Text style={styles.title}>Hello TestyMcTest!!!</Text>
+      <Text style={styles.content}>Some text below the title</Text>
+      {/* <Button>Learn More</Button> */}
     </View>
   );
 }
@@ -14,8 +18,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aquamarine',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 30
+  },
+  content: {
+    marginLeft: '15%',
+    marginRight: '15%',
+    marginTop: '4%',
+    marginBottom: '8%'
+  }
 });
+
