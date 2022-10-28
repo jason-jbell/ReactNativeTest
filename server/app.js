f4a60497f5a14c3cda9e4ece42c8c035b85ab1ea
JasonBell <jason.jbell@yahoo.com>
2022-10-27 19:02:39 -0700

    added dotenv

--- server/app.js
+++ server/app.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./Store');
+const pass = process.env.PASSWORD;
+app.use(bodyParser.json());
 
 const Store = mongoose.model('store');
 
-const uri =
-  'mongodb+srv://Jasonjbell:5rPoMbT2dJrCZAiK@pos365-jasonbell.l36uwdx.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(uri);
+const uri = `mongodb+srv://Jasonjbell:${pass}@pos365-jasonbell.l36uwdx.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.connection.on('connected', () => {
   console.log('connected to mongo');
 });
 mongoose.connection.on('error', (error) => {
   console.error(error);
 });
+
+app.get('/', (req, res) => {
+  res.send(App);
+});
+
+app.post('/send', (req, res, next) => {
+  const curr = req.body;
+  const store = new Store({
+    name: curr.name,
+    phone: curr.phone,
+    address: curr.address,
+    status: curr.status,
+  });
+  try {
+    store.save();
+    res.send('successful post');
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Fuckin it up on port 3000');
+});
+
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // const client = new MongoClient(uri, {
@@ -27,11 +57,3 @@ mongoose.connection.on('error', (error) => {
 //   // perform actions on the collection object
 //   client.close();
 // });
-
-app.get('/', (req, res) => {
-  res.send(App);
-});
-
-app.listen(3000, () => {
-  console.log('Fuckin it up on port 3000');
-});
